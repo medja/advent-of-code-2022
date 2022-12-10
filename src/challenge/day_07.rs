8e708ca93f17bb81d36d12766c707927aeedf70e
Metod Medja <metod@medja.net>
2022-12-10 07:18:03 +0100

    Optimize Day 07 by assuming DFS
    It turns out that the commands execute a DFS traversal of the file
    system. This greatly simplifies directory tracking.

--- src/challenge/day_07.rs
+++ src/challenge/day_07.rs
@@ -1,71 +1,46 @@
-use std::collections::HashMap;
-
 pub fn part_a(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
-    let result = compute_directory_sizes(input)?
-        .values()
-        .filter(|size| **size <= 100000)
+    let result = compute_directory_sizes(input)
+        .into_iter()
+        .filter(|size| *size <= 100000)
         .sum::<usize>();
 
     Ok(result)
 }
 
 pub fn part_b(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
-    let root: &[&str] = &[];
-    let directories = compute_directory_sizes(input)?;
-    let min_size = directories.get(root).unwrap() - 40000000; // (+ 30000000 - 70000000)
+    let directories = compute_directory_sizes(input);
+    let min_size = directories[0] - 40000000; // (+ 30000000 - 70000000)
 
-    let result = *directories
-        .values()
-        .filter(|size| **size >= min_size)
+    let result = directories
+        .into_iter()
+        .filter(|size| *size >= min_size)
         .min()
         .unwrap();
 
     Ok(result)
 }
 
-fn compute_directory_sizes<'a>(input: &[&'a str]) -> anyhow::Result<HashMap<Vec<&'a str>, usize>> {
+// Turns out the commands do a DFS traversal of the file system
+fn compute_directory_sizes(input: &[&str]) -> Vec<usize> {
     let mut working_directory = Vec::with_capacity(16);
-    let mut directory_sizes = HashMap::new();
+    let mut directory_sizes = Vec::with_capacity(200);
 
     for line in input {
         if line.starts_with(|char: char| char.is_ascii_digit()) {
-            let size = line
-                .split_ascii_whitespace()
-                .next()
-                .unwrap()
-                .parse::<usize>()?;
+            let size = line.split_once(' ').unwrap().0.parse::<usize>().unwrap();
 
-            for i in 0..=working_directory.len() {
-                match directory_sizes.get_mut(&working_directory[..i]) {
-                    Some(sum) => *sum += size,
-                    None => {
-                        directory_sizes.insert(working_directory[..i].to_vec(), size);
-                    }
-                }
+            for &i in &working_directory {
+                directory_sizes[i] += size;
             }
         } else if line.starts_with("$ cd") {
-            change_directory(&mut working_directory, &line[5..]);
+            if &line[5..] == ".." {
+                working_directory.pop();
+            } else {
+                working_directory.push(directory_sizes.len());
+                directory_sizes.push(0);
+            }
         }
     }
 
-    Ok(directory_sizes)
-}
-
-fn change_directory<'a>(working_directory: &mut Vec<&'a str>, mut path: &'a str) {
-    if path.starts_with('/') {
-        working_directory.clear();
-        path = &path[1..];
-    }
-
-    if path.is_empty() {
-        return;
-    }
-
-    for segment in path.split('/') {
-        if segment == ".." {
-            working_directory.pop();
-        } else {
-            working_directory.push(segment);
-        }
-    }
+    directory_sizes
 }
